0d5feef21f491bc4329a53307477d0dc38bd96ec
bxbao87 <bxbao18@apcs.vn>
2023-04-10 23:09:32 +0700

    [part4a] ex4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,16 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, blog) => acc+blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((ret, blog) => {
+        if (ret === null || ret.likes < blog.likes)
+            return blog
+
+        return ret
+    }, null)
+}
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
