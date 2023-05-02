af48f1fe431e5d1090a8ce175aebdf9a11ae8d21
bxbao87 <bxbao18@apcs.vn>
2023-05-02 22:16:00 +0700

    [part4d] ex4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,36 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/',  async (request, response) => {
-    let blogs = await Blog.find({})
+    let blogs = await Blog
+        .find({}).populate('user', {
+            username: 1,
+            name: 1,
+        })
     response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
-    let savedBlog = await blog.save()
+    const body = request.body
+
+    const user = await User.findOne()
+    if (!user) {
+        return response.status(401).send({ error: 'No user found' })
+    }
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user.id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response.status(201).json(savedBlog)
 })
 
